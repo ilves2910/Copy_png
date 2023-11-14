4e7bf26a0eb7058bf5b22f0a3e8174cafb07cf94
Elena Muratova <ilves1@yandex.ru>
2023-11-14 10:12:08 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try(FileInputStream fileInputStream = new FileInputStream("star.png");
+            FileOutputStream fileOutputStream = new FileOutputStream("star_new.png")
+        ){
+            byte[] bytes = new byte[256];
+            int count;
+            while ((count=fileInputStream.read(bytes))!=-1){
+                fileOutputStream.write(bytes, 0, count);
+            }
+            System.out.println("DONE");
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+
     }
 }
